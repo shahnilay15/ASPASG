fb5562572e70f65dd558d60e5dd0293cc39b7cbf
nilay <niknil56@gmail.com>
2021-04-04 15:41:27 +0530

    ass1

---
+++ Test/Test/Test/Controllers/ICBController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Test.Controllers
+{
+    [Authorize]
+    public class ICBController : Controller
+    {
+        private readonly ICB _number;
+
+        public ICBController(ICB number)
+        {
+            _number = number;
+        }
+        private static int i;
+
+        public IActionResult Index(string guess)
+        {
+            String AllowedChars = @"[0-9]";
+
+            if (guess != null && (Regex.IsMatch(guess, AllowedChars)))
+            {
+
+
+                string str = "You Won";
+
+                var res = _number.Compare(guess);
+               
+
+                ViewBag.message = $"{res}";
+
+                return View();
+            }
+            else
+            {
+
+                return View();
+            }
+
+
+        }
+    }
+
+}
